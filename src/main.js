7baeb15b828d751669579fb33d784d6d1690b2fa
cxy <991454464@qq.com>
2020-04-22 18:03:08 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+import '@/styles/index.scss' // global css
+
+Vue.use(ElementUI, { size: 'small' });
 Vue.config.productionTip = false
 
 new Vue({
