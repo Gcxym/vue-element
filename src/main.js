b1ba98eb4621e84039386d56b21821f4eb561a96
cxy <991454464@qq.com>
2020-05-22 17:46:40 +0800

    init

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import App from './App'
 import store from './store'
 import router from './router'
 
-import '@/icons' // icon
+import '@/assets/icons' // icon
 import '@/permission' // permission control
 
 /**
